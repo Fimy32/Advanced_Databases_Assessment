e24f1e386320183972ce93daf15e6612f23dc3af
Ben Haigh-Shipp <0haigb36@solent.ac.uk>
2025-11-24 10:33:22 +0000

    Added an extra function

--- ecommerceDBHandler.py
+++ ecommerceDBHandler.py
@@ -379,31 +379,21 @@ def simulatePurchases():
     CustomerBasketPaid(13)
     DB.commit()
 
-def send_media_to_sql():
-    file_path_name = 'C:\\Users\petrosyana\Downloads\Music.mp3'
+def send_media_to_sql(filePath):
     #To open a file in binary format, add 'b' to the mode parameter and "rb" mode opens the file in binary format for reading.
-    with open(file_path_name, 'rb') as file:
+    with open(filePath, 'rb') as file:
         file_blob = file.read()
     #to see file in BLOB values
     print("[INFO] : the last 100 characters of blob = ", file_blob[:100]) 
-    try:
-        conn = sqlite3.connect('db1')
-        print("[INFO] : Successful connection!")
-        cur = conn.cursor()
-        #to insert file_path_name and BLOB to database audio_table
-        sql_insert_file_query = "INSERT INTO audio_table(file_path_name, file_blob)VALUES(?, ?)"
-        cur = conn.cursor()
-        cur.execute(sql_insert_file_query, (file_path_name, file_blob, ))
-        conn.commit()
-        #message to test
-        print("[INFO] : The blob for ", file_path_name, " sent and saved in the database audio_table.") 
-    except Error as e:
-        print(e)
-    finally:
-        if conn:
-            conn.close()
-        #else:
-            #error = "Oh shucks, something is wrong here."
+
+    #to insert file_path_name and BLOB to database audio_table
+    sql_insert_file_query = "INSERT INTO audio_table(filePath, file_blob)VALUES(?, ?)"
+
+    cursor.execute(sql_insert_file_query, (filePath, file_blob, ))
+    DB.commit()
+    #message to test
+    print("[INFO] : The blob for ", filePath, " sent and saved in the database audio_table.") 
+
 
 
 
